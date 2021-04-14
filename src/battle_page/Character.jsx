1d377e24be5afc23dd46b42644243b2a716df94b
FlugT <flugtomer@gmail.com>
2021-04-14 17:56:03 +0300

    cssing

--- src/battle_page/Character.jsx
+++ src/battle_page/Character.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from './Character.module.scss'
+import clsx from 'clsx'
 
-export const Character = ({name, image, maxHP, hp}) => {
-  return <div>
-    <div>{name}</div> 
-    <div>{image}</div> 
-    <div>{maxHP}</div> 
-    <div>{hp}</div>
+export const Character = ({name, image, maxHP, hp, faction, variant}) => {
+  return <div className={clsx([css.cardContainer, css[variant], css[faction]])}>
+    <div className={css.characterName}>{name}</div> 
+    <img className={css.avatar} src={image}/>
+    <div className={css.characterInfo}>{hp} / {maxHP}</div>
   </div>;
 };
 
@@ -15,8 +16,10 @@ Character.propTypes = {
   image: PropTypes.string,
   maxHP: PropTypes.number.isRequired,
   hp: PropTypes.number.isRequired,
+  faction: PropTypes.oneOf(['ally', 'enemy', 'neutral']).isRequired,
+  variant: PropTypes.oneOf(['horizontal', 'vertical']),
 };
 
 Character.defaultProps = {
-  
+  variant: 'horizontal',
 };
